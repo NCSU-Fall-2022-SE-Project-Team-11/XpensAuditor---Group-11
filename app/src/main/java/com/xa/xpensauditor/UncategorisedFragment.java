9e8f87042d0c8b3726343decd4deb3a5322c3ba5
vemuriPradyumna <pradyumna.vemuri@gmail.com>
2022-12-04 15:14:20 -0500

    Javadocs for UncategorizedFragment

--- app/src/main/java/com/xa/xpensauditor/UncategorisedFragment.java
+++ app/src/main/java/com/xa/xpensauditor/UncategorisedFragment.java
@@ -35,7 +35,9 @@ import java.util.ArrayList;
 import java.util.List;
 import java.text.DateFormatSymbols;
 
-
+/**
+ * Manages uncategorized transactions of the user
+ */
 public class UncategorisedFragment extends Fragment {
 
     private String tagId, catChangeTo, tagDate;
@@ -54,7 +56,11 @@ public class UncategorisedFragment extends Fragment {
 
     private TransactionAdapter mAdapterUF;
 
-
+    /**
+     * Handles Fragments and transcation in the main page based on the position
+     * @param position
+     * @return
+     */
     public static Fragment getInstance(int position) {
         Bundle bundle = new Bundle();
         bundle.putInt("pos2", position);
@@ -62,6 +68,10 @@ public class UncategorisedFragment extends Fragment {
         uncategorisedFragment.setArguments(bundle);
         return uncategorisedFragment;
     }
+
+    /**
+     * Constructor
+     */
     public UncategorisedFragment() {
         // Required empty public constructor
         int i=0;
@@ -1087,7 +1097,13 @@ public class UncategorisedFragment extends Fragment {
         i++;
     }
 
-
+    /**
+     * Loads data when fragment is loaded
+     * @param inflater
+     * @param container
+     * @param savedInstanceState
+     * @return
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -1095,6 +1111,11 @@ public class UncategorisedFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_uncategorised, container, false);
     }
 
+    /**
+     * Loads data when view is created.
+     * @param view
+     * @param savedInstanceState
+     */
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
