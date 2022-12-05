6e750714c32bb525f1758d6f930ed3d2942ccd59
aditiv-79 <aditivakeel@gmail.com>
2022-12-05 16:42:44 -0500

    update limtmail

--- app/src/main/java/com/xa/xpensauditor/LimitEmailSender.java
+++ app/src/main/java/com/xa/xpensauditor/LimitEmailSender.java
@@ -144,6 +144,13 @@ public class LimitEmailSender extends AsyncTask<Void, Integer, Boolean> {
         i++;
         i++;
         i++;
+        i++;
+        i++;
+        i++;
+        i++;
+        i++;
+        i++;
+        i++;
 
         return sendLimitEmailUpdate("");
 
